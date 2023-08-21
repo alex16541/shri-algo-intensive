ee614b1998694ee4eb98e1955e4b27b833e65ac1
alex16541 <aleks00.7@bk.ru>
2023-08-21 18:37:18 +0300

    bouble sort

---
+++ bouble.js
@@ -0,0 +1,34 @@
+function sort(arr) {
+    let i = 0;
+    let j = 1;
+
+    let len = arr.length;
+
+    while( len >= 2) {
+        if(arr[i] > arr[j]){
+            [
+                arr[i], arr[j]
+            ] = [
+                arr[j], arr[i]
+            ]
+        }
+
+        if(j === len - 1){
+            i = 0;
+            j = 1;
+            len--;
+
+        } else {
+            i++;
+            j++;
+            
+        }
+
+    }
+
+    return arr;
+}
+
+const res = sort([1,6,3,9,5,3,4,9]);
+
+console.log(res);
